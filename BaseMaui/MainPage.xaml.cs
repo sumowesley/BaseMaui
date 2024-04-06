fd7ccf30f0d0c90790d331fc9c4fe53acc6b3015
Josh <josh@fivesixsolutions.co.uk>
2024-04-06 21:21:49 +0100

    data trigger for insets

--- BaseMaui/MainPage.xaml.cs
+++ BaseMaui/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace BaseMaui
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Core.Platform;
+
+namespace BaseMaui
 {
     public partial class MainPage : ContentPage
     {
@@ -9,23 +12,20 @@
             InitializeComponent();
         }
 
-        //private void OnCounterClicked(object sender, EventArgs e)
-        //{
-        //    count++;
-
-        //    if (count == 1)
-        //        CounterBtn.Text = $"Clicked {count} time";
-        //    else
-        //        CounterBtn.Text = $"Clicked {count} times";
-
-        //    SemanticScreenReader.Announce(CounterBtn.Text);
-        //}
+        // protected override void OnNavigatedTo(NavigatedToEventArgs args)
+        // {
+        //     base.OnNavigatedTo(args);
+        //     CommunityToolkit.Maui.Core.Platform.StatusBar.SetColor(Color.FromArgb("#F64747"));
+        //     CommunityToolkit.Maui.Core.Platform.StatusBar.SetStyle(StatusBarStyle.DarkContent);
+            
+        // }
 
         private void OnComplete(object sender, EventArgs e)
         {
             Entry entry = (Entry)sender;
             entry.Unfocus();
         }
+
     }
 
 }
