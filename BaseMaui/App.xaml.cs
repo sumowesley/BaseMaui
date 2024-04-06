e7f6340a0da1b437c03b45b7e8e33f3b8a377bdc
Karl Southern <karl@imageexcellence.co.uk>
2024-04-06 16:01:28 +0100

    Add project files.

---
+++ BaseMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BaseMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BaseMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BaseMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
