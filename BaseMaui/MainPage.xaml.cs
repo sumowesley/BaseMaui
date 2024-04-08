f9bcfa8a67e1776deeffa054673913968bca00ec
Josh <josh@fivesixsolutions.co.uk>
2024-04-08 14:55:08 +0100

    Is overlap code added, and test page changed to mock MOSS layout

--- BaseMaui/MainPage.xaml.cs
+++ BaseMaui/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Maui.Core;
+﻿using System.Diagnostics;
+using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Maui.Core.Platform;
 
 namespace BaseMaui
@@ -24,6 +25,44 @@ namespace BaseMaui
         {
             Entry entry = (Entry)sender;
             entry.Unfocus();
+        }
+
+        private void GetPosition(object sender, TappedEventArgs e)
+        {   
+            Point pos1 = GetAbsolutePosition(ContentArea);
+            Point pos2 = GetAbsolutePosition(VATStatusPageFooter);
+
+            // Check if the views overlap in the y coordinates
+            var overlap = (pos1.Y < pos2.Y + VATStatusPageFooter.Frame.Height) && (pos1.Y + ContentArea.Frame.Height > pos2.Y);
+            Debug.WriteLine(overlap ? "Overlaps" : "Doesn't Overlap");
+
+            if (overlap) {
+                VATStatusScrollView.ZIndex = 2;
+                ButtonRow.IsVisible = true;
+                VATStatusPageFooter.Opacity = 0;
+            }
+            else
+            {
+                VATStatusScrollView.ZIndex = 1;
+                ButtonRow.IsVisible = false;
+                VATStatusPageFooter.Opacity = 1;
+            }
+        }
+
+        public Point GetAbsolutePosition(View view)
+        {
+            double x = view.Frame.X;
+            double y = view.Frame.Y;
+
+            var parent = view.Parent;
+            while (parent is View parentView)
+            {
+                x += parentView.Frame.X;
+                y += parentView.Frame.Y;
+                parent = parentView.Parent;
+            }
+
+            return new Point(x, y);
         }
 
     }
