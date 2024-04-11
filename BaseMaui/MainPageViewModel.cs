3dfdd974a06f90f8293ccde505c9ba1b3d3e2ae1
Karl Southern <karl@imageexcellence.co.uk>
2024-04-11 12:26:19 +0100

    Initial navigation from main page
    The most significant changes involve the `MainPageViewModel` class. A new private field `_navigationService` of type `INavigationService` has been added. The constructor of this class has been modified to accept an `INavigationService` parameter, which is then assigned to the newly added field. The `NextPage` method has also been updated to navigate to a new page of type `TwoPage` instead of yielding control back to the caller.
    
    List of changes:
    
    1. Addition of `Microsoft.Maui.Controls.Xaml.Internals` and `Nalu` namespaces to the import statements at the top of the file. This change suggests that the file now uses classes or functions defined in these namespaces.
    
    2. Addition of a private readonly field `_navigationService` of type `INavigationService` to the `MainPageViewModel` class. This field is likely used to handle navigation between different pages in the application.
    
    3. Modification of the `MainPageViewModel` constructor to accept an `INavigationService` parameter named `navigationService`. This parameter is assigned to the `_navigationService` field, indicating that the navigation service used by the `MainPageViewModel` is now configurable.
    
    4. Modification of the `NextPage` method. The `await Task.Yield();` line has been commented out and replaced with `_navigationService.GoToAsync(Navigation.Relative().Push<TwoPage>());`. This change indicates that the `NextPage` method now navigates to a new page of type `TwoPage` instead of simply yielding control back to the caller.

--- BaseMaui/MainPageViewModel.cs
+++ BaseMaui/MainPageViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Controls.Xaml.Internals;
+using Nalu;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +12,19 @@ namespace BaseMaui
 {
     public partial class MainPageViewModel : ObservableObject
     {
+        readonly INavigationService _navigationService;
 
-        public MainPageViewModel()
+        public MainPageViewModel(INavigationService navigationService)
         {
-        
+            _navigationService = navigationService;
         }
 
 
         [RelayCommand]
         public async Task NextPage()
         {
-            await Task.Yield();
+            // await Task.Yield();
+            await _navigationService.GoToAsync(Navigation.Relative().Push<TwoPage>());
         }
 
     }
