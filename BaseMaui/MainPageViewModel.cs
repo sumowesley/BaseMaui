bf5cbea5e82da8e1de37f58db188b39b8477904d
Josh <josh@fivesixsolutions.co.uk>
2024-05-02 14:07:20 +0100

    Messing with TouchBehavior and buttons

--- BaseMaui/MainPageViewModel.cs
+++ BaseMaui/MainPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -52,14 +53,14 @@ namespace BaseMaui
         [RelayCommand]
         public void StrikeOne()
         {
-
+            Debug.WriteLine("StrikeOne");
         }
 
 
         [RelayCommand]
-        public void StrikwTwo()
+        public void StrikeTwo()
         {
-
+            Debug.WriteLine("StrikeTwo");
         }
 
     }
